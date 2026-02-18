a972c050db99ae80414920b5c53b71ae04e8474e
Brandon Lee <brandon-kf-lee@gmail.com>
2026-02-18 12:41:29 -0800

    Add support for DMA, add error checking for message forwarding

--- hw/misc/sc_dev.c
+++ hw/misc/sc_dev.c
@@ -10,6 +10,7 @@
 #include "qemu/log.h"			 /* Error logs */
 #include "hw/sysbus.h"  		 /* Provides all sysbus registering functions */
 #include "hw/qdev-properties.h"  /* QEMU device properties */
+#include "system/dma.h"          /* DMA functions */
 
 #include "hw/misc/sc_dev.h"
 
@@ -20,12 +21,32 @@ DECLARE_INSTANCE_CHECKER(ScDevState, SC_DEV, TYPE_SC_DEV)
 #define REG_ID 	0x0
 #define CHIP_ID	0xBA000001
 
+/* DMA */
+#define DMA_BUFFER_SIZE 4096   // 4KB
+
+#define DMA_CONTROL   0x20
+#define DMA_SRC_LO    0x24
+#define DMA_SRC_HI    0x28
+#define DMA_DST       0x2C
+#define DMA_LEN       0x30 
+#define DMA_STAT      0x34 
+
+#define DMA_START     (1 << 0)
+#define DMA_BUSY      (1 << 0)
+#define DMA_DONE      (1 << 1)
+#define DMA_ERR       (1 << 2)
+
 /* Forward declarations */
 static void sc_dev_instance_init(Object *obj);
 static void sc_dev_realize(DeviceState *dev, Error **errp);
 static void sc_dev_unrealize(DeviceState *dev);
+
+static int8_t sc_dev_tlm_transaction(ScDevState *state, 
+									   uint8_t is_write, uint8_t is_dma, 
+                                       uint64_t addr, void *buf, uint32_t size);
 static uint64_t sc_dev_read(void *opaque, hwaddr addr, unsigned int size);
 static void sc_dev_write(void *opaque, hwaddr addr, uint64_t data, unsigned int size);
+static void sc_dev_dma(ScDevState *state);
 
 /* Device state structure 
    Keep track of the status of the device in this board. Specific registers & IRQs may be defined here 
@@ -33,56 +54,84 @@ static void sc_dev_write(void *opaque, hwaddr addr, uint64_t data, unsigned int 
 struct ScDevState {
 	SysBusDevice parent_obj;
 	MemoryRegion iomem;  // Memory mapped space for the device
-	uint64_t chip_id;
+    AddressSpace *dma_as; // DMA address space
+	
+    uint64_t chip_id;
 
-	/* Socket connection to SystemC */
+    /* DMA registers */
+    uint64_t dma_src;
+    uint64_t dma_dst;
+    uint32_t dma_len;
+    uint32_t dma_ctrl;
+    uint32_t dma_stat;
+
+	/* Socket connection to external device */
     int sc_sock;
     char *socket_path;
 };
 
-/* Stucture of a TLM message */
-struct tlm_msg {
+struct bridge_msg {
     uint8_t  is_write;
+    uint8_t  is_dma;
+    uint16_t reserved; /* Ensure word alignment */
+
     uint64_t addr;
     uint32_t size;
-    uint64_t data;
+
+    uint8_t  data[DMA_BUFFER_SIZE];
+
+    int8_t status;
 } __attribute__((packed));  /* Ensure no padding */
 
 
-/* Helper: Send and receive TLM transactions */
-static uint64_t sc_dev_tlm_transaction(ScDevState *state, 
-									   uint8_t is_write, uint64_t addr, uint32_t size, uint64_t data)
+/* Helper: Send and receive messages formatted for TLM transactions. 
+   Returns status of transaction, forwarded from SystemC
+ */
+static int8_t sc_dev_tlm_transaction(ScDevState *state, 
+									   uint8_t is_write, uint8_t is_dma, 
+                                       uint64_t addr, void *buf, uint32_t size)
 {
-    struct tlm_msg msg = {
+    struct bridge_msg msg = {
         .is_write = is_write,
+        .is_dma = is_dma,
         .addr = addr,
         .size = size,
-        .data = data,
-    };
-    struct tlm_msg response;
+        .status = 0,  /* Code for incomplete message */
+    };    
+    struct bridge_msg response;
     ssize_t ret;
 
-	/* Ensure connection */
-    if (state->sc_sock < 0) {
-        qemu_log_mask(LOG_GUEST_ERROR, "sc_dev: socket not connected\n");
-        return 0xDEADBEEF;
+    /* Copy data buffer into message if writing */
+    if (is_write && buf) {
+        memcpy(msg.data, buf, size);
     }
 
-    /* Send request */
+	/* Ensure bridge socket connection */
+    if (state->sc_sock < 0) {
+        qemu_log_mask(LOG_GUEST_ERROR, "sc_dev: socket not connected\n");
+        return -1;
+    }
+
+    /* Send request, error if message wasn't sent */
     ret = send(state->sc_sock, &msg, sizeof(msg), 0);
     if (ret != sizeof(msg)) {
         qemu_log_mask(LOG_GUEST_ERROR, "sc_dev: failed to send TLM message\n");
-        return 0xDEADBEEF;
+        return -1;
     }
 
-    /* Receive response */
+    /* Receive response, error if nothing was received */
     ret = recv(state->sc_sock, &response, sizeof(response), MSG_WAITALL);
     if (ret != sizeof(response)) {
         qemu_log_mask(LOG_GUEST_ERROR, "sc_dev: failed to receive TLM response\n");
-        return 0xDEADBEEF;
+        return -1;
     }
-
-    return response.data;
+    
+    /* Copy response data back to caller's buffer (for reads) */
+    if (!is_write && buf) {
+        memcpy((void*)buf, response.data, size);
+    }
+    
+    return response.status;
 }
 
 /* Memory read callback */
@@ -91,27 +140,141 @@ static uint64_t sc_dev_read(void *opaque, hwaddr addr, unsigned int size)
     ScDevState *state = SC_DEV(opaque);
 
     switch (addr) {
-        case REG_ID:
-            return state->chip_id;
+        //case REG_ID:
+        //    return state->chip_id;
+
+        case DMA_STAT:
+            return state->dma_stat;
 
         default:
-            /* Forward transaction to SystemC */
-            return sc_dev_tlm_transaction(state, 0, addr, size, 0);
+            /* Forward transaction to SystemC and receive response */
+            uint8_t buf[8] = {0};  // Stack buffer for read data
+            uint32_t data = 0;     // First 4 control bytes
+            int8_t ret;            // TLM transaction status (tlm::tlm_response_status)
+            
+            if((ret = sc_dev_tlm_transaction(state, 0, 0, addr, buf, size)) < 0) {
+                qemu_log_mask(LOG_GUEST_ERROR,
+                              "sc_dev: TLM read error at 0x%lx: %d\n",
+                              (unsigned long)addr, ret);
+                return 0; // TODO: figue out a way to return the TLM error number or some other defined (non-negative) error value
+            }
+            memcpy(&data, buf, sizeof(uint32_t));
+            return data;
     }
 }
 
 /* Memory write callback */
-static void sc_dev_write(void *opaque, hwaddr addr, uint64_t data, unsigned int size)
+static void sc_dev_write(void *opaque, hwaddr addr,
+                         uint64_t data, unsigned int size)
 {
     ScDevState *state = SC_DEV(opaque);
 
-    /* Forward all writes to SystemC */
-    sc_dev_tlm_transaction(state, 1, addr, size, data);
+    switch (addr) {
+        /* DMA registers */
+        case DMA_CONTROL:
+            state->dma_ctrl = data;
+
+            if (data & DMA_START) {
+                sc_dev_dma(state);
+            }
+            break;
+
+        case DMA_SRC_LO:
+            state->dma_src &= 0xffffffff00000000ULL;
+            state->dma_src |= (uint32_t)data;
+            break;
+
+        case DMA_SRC_HI:
+            state->dma_src &= 0x00000000ffffffffULL;
+            state->dma_src |= ((uint64_t)data << 32); 
+            break;
+
+        case DMA_DST:
+            state->dma_dst = data;
+            break;
+
+        case DMA_LEN:
+            state->dma_len = data;
+            break;
+
+        /* Control registers (Write but not DMA) */
+        default:
+            uint8_t buf[8] = {0};  // Buffer for write data
+            int8_t ret;            // TLM transaction status
+
+            memcpy(buf, &data, size);
+            
+            if ((ret = sc_dev_tlm_transaction(state, 1, 0, addr, buf, size)) < 0) {
+                qemu_log_mask(LOG_GUEST_ERROR,
+                              "sc_dev: TLM write error at 0x%lx: %d\n",
+                              (unsigned long)addr, ret);
+            }
+            break;
+    }
+}
+
+/* Use DMA to access data stored in memory, then send to CIM device 
+   Registers must be set before use:
+                          Source address (data to write)
+                          Dest address (where to write to)
+                          Data length
+*/
+
+static void sc_dev_dma(ScDevState *state)
+{
+    uint8_t buf[DMA_BUFFER_SIZE];    // Temporary storage for data read from memory
+    uint64_t addr = state->dma_src;  // Guest address to read
+    uint64_t dest = state->dma_dst;  // Device address to write into
+    uint32_t left = state->dma_len;  // Total length of read
+    uint32_t chunk = 0;              // Chunk size a single TLM transaction
+    MemTxResult result;              // Result of DMA memory read
+
+    /* TODO: Test to make sure DMA doesn't run if not all three registers are populated */
+    if (!addr || !dest || !left) {
+        qemu_log_mask(LOG_GUEST_ERROR, "dma_src, dma_dst, or dma_len not set.\n");
+        state->dma_stat = DMA_ERR;
+        return;
+    }    
+    
+    state->dma_stat = DMA_BUSY;
+
+    while (left) {
+        chunk = MIN(left, DMA_BUFFER_SIZE);
+
+        /* Read guest RAM */
+        result = dma_memory_read(state->dma_as, addr, buf, chunk, MEMTXATTRS_UNSPECIFIED);
+
+        if (result != MEMTX_OK) {
+            qemu_log_mask(LOG_GUEST_ERROR,
+                          "sc_dev: DMA read failed at 0x%lx: %d\n",
+                          (unsigned long)addr, result);
+            state->dma_stat = DMA_ERR;
+            return;
+        }
+
+        /* Send to SystemC the chunked buffer data */
+        if (sc_dev_tlm_transaction(state, 1, 1, dest, buf, chunk) < 0) {
+            state->dma_stat = DMA_ERR;
+            return;
+        }
+
+        /* Increment addresses and decrement remaining chunks */
+        addr += chunk;
+        dest += chunk;
+        left -= chunk;
+    }
+
+    state->dma_stat = DMA_DONE;
+    
+    /* Zero out DMA registers when finished */
+    state->dma_src = 0;  
+    state->dma_dst = 0;  
+    state->dma_len = 0;  
 }
 
 
 /* File operations associated with the device
-   “Eg. When CPU reads this memory region, call sc_dev_read()”
+   “E.g. When CPU reads this memory region, call sc_dev_read()”
 */
 static const MemoryRegionOps sc_dev_ops = {
 	.read = sc_dev_read,
@@ -119,7 +282,7 @@ static const MemoryRegionOps sc_dev_ops = {
 	.endianness = DEVICE_NATIVE_ENDIAN,
 };
 
-/* Device realization. Socket is connected here 
+/* Device realization.
  */
 static void sc_dev_realize(DeviceState *dev, Error **errp)
 {
@@ -142,8 +305,20 @@ static void sc_dev_realize(DeviceState *dev, Error **errp)
         state->sc_sock = -1;
         return;
     }
-
+    
     qemu_log("sc_dev: connected to SystemC at %s\n", path);
+
+    // Declare DMA address space
+    state->dma_as = &address_space_memory;
+    
+    // Initialize registers
+    state->dma_src = 0;  
+    state->dma_dst = 0;  
+    state->dma_len = 0;  
+    state->dma_ctrl = 0;  
+    state->dma_stat = 0; 
+    
+    qemu_log("sc_dev: DMA intialized");
 }
 
 /* Device unrealization (socket cleanup) */
@@ -160,7 +335,6 @@ static void sc_dev_unrealize(DeviceState *dev)
 /* Device properties */
 static const Property sc_dev_properties[] = {
     DEFINE_PROP_STRING("socket-path", ScDevState, socket_path),
-    //DEFINE_PROP_END_OF_LIST(),
 };
 
 /* Initialize device state */
